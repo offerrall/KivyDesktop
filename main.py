91a4d1b882639d898019ec0d3fc3c39f6ce6eabe
offerrall <offerrallps4@gmail.com>
2025-03-12 09:28:54 +0100

    Refactor button creation to use a lambda for callback and simplify button addition to DScrollView

--- main.py
+++ main.py
@@ -4,25 +4,19 @@ from kivy_desktop.scroll import DScrollView
 from kivy_desktop.app import DApp
 
 
-def button_callback(button):
-    print(f"Botón presionado: {button.text}")
-
-def add_buttons(container):
-
-    for i in range(5):
-
-        btn = DButton(
-            text=f"Botón {i}",
-            size_hint_y=None,
-            height=dp(50),
-            release_callback=button_callback
-        )
-
-        container.add_widget(btn)
-
 
 scroll_view = DScrollView()
-add_buttons(scroll_view)
+
+for i in range(5):
+
+    btn = DButton(text=f"Botón {i}",
+                  size_hint_y=None,
+                  height=dp(50),
+                  release_callback=lambda instance: print(instance.text)
+                  )
+
+    scroll_view.add_widget(btn)
+
 app = DApp(main_container=scroll_view)
 
 if __name__ == "__main__":
