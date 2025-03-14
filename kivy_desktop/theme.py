3d681c9e04bd4d7cc6a5b958d3b5b1af5aee5831
offerrall <offerrallps4@gmail.com>
2025-03-14 13:39:27 +0100

    aa

--- kivy_desktop/theme.py
+++ kivy_desktop/theme.py
@@ -3,7 +3,6 @@ from kivy.metrics import dp
 COLORS = {
     'back1': [0.10, 0.10, 0.10, 1],
     'back2': [0.05, 0.05, 0.05, 1],
-    
     'seleted': [0, 1, 1, 1],
     'error': [0.7, 0, 0, 1],
     'font': [1, 1, 1, 1],
